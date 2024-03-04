b4548d002ce9ffbb467b7c46cb3f5d86619b7f71
Hafiz Usman Mahmood <um2005@hw.ac.uk>
2024-03-04 19:30:49 +0000

    Added baggage class

---
+++ src/Baggage.java
@@ -0,0 +1,41 @@
+public class Baggage  {
+    private Double weight;
+    private Double height;
+    private Double length;
+    private Double breath;
+
+    public Baggage(Double weight, Double height, Double length, Double breath) {
+        if (weight == null || height == null || length == null || breath == null) {
+            throw new IllegalArgumentException("Parameters cannot be null");
+        }
+
+        if (weight < 0 || height < 0 || length < 0 || breath < 0) {
+            throw new IllegalArgumentException("Dimensions cannot be negative");
+        }
+        this.weight = weight;
+        this.height = height;
+        this.length = length;
+        this.breath = breath;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public Double getLength() {
+        return length;
+    }
+
+    public Double getBreath() {
+        return breath;
+    }
+
+    public Double getVolume( ){
+        return height * length * breath;
+    }
+
+    public Double getWeight( ) {
+        return weight;
+    }
+
+}
