24a5cbc8d356d86d47389684788c6460f911316d
Jacobs Ikedi <idj2001@hw.ac.uk>
2024-03-04 19:28:31 +0000

    Included passenger class Included passenger repository

---
+++ src/Passenger.java
@@ -0,0 +1,52 @@
+public class Passenger {
+    private final String bookingRef;
+    private final String flightCode;
+    private final String firstName;
+    private final String lastName;
+    private Boolean checkedIn;
+
+    public Passenger(String bookingRef, String flightCode, String firstName, String lastName, Boolean checkedIn) {
+
+        if (bookingRef == null || flightCode == null || firstName == null || lastName == null || checkedIn == null) {
+            throw new IllegalArgumentException("Parameters cannot be null");
+        }
+
+
+        if (bookingRef.trim().isEmpty() || flightCode.trim().isEmpty() || firstName.trim().isEmpty() || lastName.trim().isEmpty()) {
+            throw new IllegalArgumentException("String parameters cannot be empty");
+        }
+
+        if (!lastName.trim().matches("[a-zA-Z:-]+") || !firstName.trim().matches("[a-zA-Z:-]+")) {
+            throw new IllegalArgumentException("Invalid name input");
+        }
+
+        this.bookingRef = bookingRef;
+        this.flightCode = flightCode;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.checkedIn = checkedIn;
+    }
+    public String getBookingRef( ) {
+        return bookingRef;
+    }
+
+    public String getFlightCode( ) {
+        return flightCode;
+    }
+
+    public String getFirstName( ) {
+        return firstName;
+    }
+
+    public String getLastName( ) {
+        return lastName;
+    }
+
+    public Boolean getCheckedIn( ) {
+        return checkedIn;
+    }
+
+    public void setCheckedIn(Boolean checkedIn) {
+        this.checkedIn = checkedIn;
+    }
+}
